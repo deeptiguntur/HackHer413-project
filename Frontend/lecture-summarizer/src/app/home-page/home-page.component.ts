b24a9c13f9d0aeab2db589c67af31e067df74bde
Deepti Guntur <deepti25.guntur@gmail.com>
2024-02-17 22:21:59 -0500

    Frontend code

---
+++ Frontend/lecture-summarizer/src/app/home-page/home-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+  url: any;
+  format: any;
+  onSelectFile(event: any) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      if(file.type.indexOf('image')> -1){
+        this.format = 'image';
+      } else if(file.type.indexOf('video')> -1){
+        this.format = 'video';
+      }
+      reader.onload = (event) => {
+        this.url = (<FileReader>event.target).result;
+        console.log(this.url)
+      }
+    }
+  }
+}
